bf25fbac3a926e07fd0c7a2bdf30ac85f3ef48e3
NotxFound <83174921+NotxFound@users.noreply.github.com>
2025-10-30 23:49:36 +0100

    Add files via upload

--- translations.js
+++ translations.js
@@ -1,37 +1,6 @@
-let translations = {};
-let currentLang = 'pl';
-
-async function loadLanguage() {
-    const response = await fetch('language.json');
-    translations = await response.json();
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-    await loadLanguage();
-
-    document.querySelectorAll('.lang-button').forEach(button => {
-        button.addEventListener('click', () => {
-            const lang = button.getAttribute('data-lang');
-
-            if (lang === 'pl') {
-                location.reload();
-            } else {
-                translatePage(lang);
-            }
-        });
-    });
-});
-
-function translatePage(language) {
-    currentLang = language;
-
-    document.querySelectorAll('[data-i18n]').forEach(elem => {
-        const key = elem.getAttribute('data-i18n');
-        if (translations[language] && translations[language][key]) {
-            elem.textContent = translations[language][key];
-        }
-    });
-    if (translations[language] && translations[language]['document.title']) {
-        document.title = translations[language]['document.title'];
-    }
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        { pageLanguage: 'pl', includedLanguages: 'en,ar,tr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
+        'google_translate_element'
+    );
 }
