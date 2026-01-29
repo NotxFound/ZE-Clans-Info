0cacd15e99a27683d851b2f2cb9a80e423a7e086
NotxFound <83174921+NotxFound@users.noreply.github.com>
2026-01-29 13:54:27 +0100

    Delete translations.js

--- translations.js
+++
@@ -1,6 +0,0 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        { pageLanguage: 'pl', includedLanguages: 'en,ar,tr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
-        'google_translate_element'
-    );
-}
